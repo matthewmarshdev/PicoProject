c4880a369efb06601de724e87dffbc4f0cba31d2
WslyStvnChng <mr.cheng.st@gmail.com^C>
2018-04-21 14:12:25 -0600

    making file structure for backend work

--- models/dog.js
+++ models/dog.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const dogSchema = new Schema({
+let dogSchema = new Schema({
   Name: { type: String, required: true },
+  Size: { type: Number, required: true },
   Breed: { type: String, required: true },
   Weight: { type: Number, required: false },
   Temperment: { type: String, required: true },
+  Sex: { type: Number, required: true },
   Story: { type: String, required: true },
   pickupDate: { type: Date, default: Date.now }
 });
