d4eb1beba275dcce7c256a207046861395900ece
Tucker <tuckerk23@gmail.com>
2018-05-02 16:15:58 -0600

    refactored dog api routes, updated utils api functions, dogController, and added steven's quasi working user front/backend submission code

--- controllers/dogsController.js
+++ controllers/dogsController.js
@@ -35,19 +35,29 @@ const db = require("../models");
 
 // Defining methods for the booksController
 module.exports = {
- 
- addDog: function(req, res) {
-   db.dog.create(req.body)
-     .then(dbModel => res.json(dbModel))
-     .catch(err => res.status(422).json(err));
- },
- getDogs: function(req, res) {
-   db.dog
-     .find({})
-     .sort({date: -1})
-     .then(dbDogs => res.json(dbDogs))
-     .catch(err => res.status(422).json(err));
-   },
-
-
+  addDog: function(req, res) {
+    db.dog
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  getDogs: function(req, res) {
+    db.dog
+      .find({})
+      .sort({ date: -1 })
+      .then(dbDogs => res.json(dbDogs))
+      .catch(err => res.status(422).json(err));
+  },
+  findDogById: function(req, res) {
+    db.dog
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  updateDogById: function(req, res) {
+    db.dog
+      .findOneAndUpdate({_id: req.params.id}, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
 };
